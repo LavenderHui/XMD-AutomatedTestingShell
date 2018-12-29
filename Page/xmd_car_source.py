eb8231d3446fb4eb611076fe819dc438e6707a57
LavenderHui <Lavender_xiaohui@163.com>
2018-12-29 10:50:39 +0800

    采车170账号门店2

--- Page/xmd_car_source.py
+++ Page/xmd_car_source.py
@@ -55,7 +55,7 @@ class search(Base):
     def  __init__(self,deiver):
         Base.__init__(self,deiver)
     # 登录采集车源账号
-    def input_login(self,name="10000000145",passwo="uat.portal"):
+    def input_login(self,name="10000000170",passwo="uat.portal"):
         allure.attach("用户登陆信息：", "用户名:%s\n密码:%s" % (name, passwo))
         self.input_element(Page.username,name)
         self.input_element(Page.password,passwo)
@@ -216,6 +216,9 @@ class search(Base):
         self.click_element(Page.jia)
         self.click_element(Page.ss)
         md = self.search_elements(Page.xzmd)
+        #mdRandom = []
+        #mdRandom = list(range(0, 3))
+        #mdRandom.click()
         md[1].click()
         self.click_element(Page.okan)
         # self.click_element(Page.baocun)
